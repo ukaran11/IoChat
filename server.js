ca08df3c6b629b135ad9e1e53cdab895fca5a1ae
Utsav <ukaran11@gmail.com>
2019-08-10 02:09:29 +0530

    commit

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ io.sockets.on('connection', function(socket){
 
     // Disconnect
     socket.on('disconnect', function(data) {
+        users.splice(users.indexOf(socket.username), 1)
+        updateUsernames()
         connections.splice(connections.indexOf(socket), 1)
         console.log('Disconnected: %s sockets connected', connections.length)
      })
@@ -27,6 +29,18 @@ io.sockets.on('connection', function(socket){
      // Send Message
      socket.on('send message', function(data){
         console.log(data)
-        io.sockets.emit('new message', {msg: data})
+        io.sockets.emit('new message', {msg: data, user: socket.username})
      })
+
+     // New User
+     socket.on('new user', function(data, callback) {
+         callback(true)
+         socket.username = data
+         users.push(socket.username)
+         updateUsernames()
+     })
+
+     function updateUsernames() {
+         io.sockets.emit('get users', users)
+     }
  })
